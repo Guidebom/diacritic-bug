1e61ea6ff33e6547740ec86f8e91cf99d5b77640
Guilherme Debom <guilherme.debom@adp.com>
2023-06-23 13:47:53 -0300

    New project

---
+++ src/test/java/com/example/diacriticalbug/DiacriticalBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diacriticalbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiacriticalBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
